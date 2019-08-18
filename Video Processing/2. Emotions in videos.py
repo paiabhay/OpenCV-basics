a9cbdc754b0d47996a879cd43d3b753f77094ccb
Abhay Pai <paiabhay1992@gmail.com>
2019-08-19 01:36:06 +0200

    Changes to main method

--- Video Processing/2. Emotions in videos.py
+++ Video Processing/2. Emotions in videos.py
@@ -16,6 +16,14 @@ import matplotlib as mpl
 mpl.use('TkAgg')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Control log message outputs
 
+<<<<<<< HEAD
+=======
+# Provide locations of files used for in below code
+video_location = '../Videos/video3.mp4'
+model_location = 'Saved_Models/model.h5'
+haar_cascade_xml = '../Haarcascades/haarcascade_frontalface_default.xml'
+
+>>>>>>> 73642eb4f47a69c964e25c821c30ae2cf82ab39b
 
 # Create a Neural Network
 def create_model():
@@ -113,6 +121,7 @@ def run_model(model_location, video_location, haar_cascade_xml):
     cv2.destroyAllWindows()
 
 
+<<<<<<< HEAD
 def main():
     # Provide locations of files used for in below code
     video_location = 'Videos/video3.mp4'
@@ -125,3 +134,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+=======
+run_model()
+>>>>>>> 73642eb4f47a69c964e25c821c30ae2cf82ab39b
